999578c6d6d2ae48e2395e67d094843356789e7c
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2023-11-20 09:18:36 +0700

    W13: Jawaban Soal 2

---
+++ week 13/src/stream_farhan/lib/stream.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.blueAccent,
+    Colors.purpleAccent,
+    Colors.brown,
+    Colors.green,
+    Colors.orange
+  ];
+}
