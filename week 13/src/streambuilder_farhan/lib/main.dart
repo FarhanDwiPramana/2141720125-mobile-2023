86f56f9706a156c8d7923db302abd8a678af63c3
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2023-11-26 22:15:59 +0700

    W13: Jawaban Soal 12

--- week 13/src/streambuilder_farhan/lib/main.dart
+++ week 13/src/streambuilder_farhan/lib/main.dart
@@ -29,13 +29,37 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  late Stream<int> numberStream;
+  @override
+  void initState() {
+    numberStream = NumberStream().getNumbers();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Stream Farhan'),
       ),
-      body: Container(),
+      body: StreamBuilder(
+        stream: numberStream,
+        initialData: 0,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print('Error!');
+          }
+          if (snapshot.hasData) {
+            return Center(
+                child: Text(
+              snapshot.data.toString(),
+              style: const TextStyle(fontSize: 96),
+            ));
+          } else {
+            return const SizedBox.shrink();
+          }
+        },
+      ),
     );
   }
 }
